75c62744d1e5e7721a975aabaf35c481170faa3a
takeda michika <mi.chi.ka10@icloud.com>
2024-06-17 10:54:32 +0900

    10.5弾を発射できるようにする

--- shotthinggame/src/Player.java
+++ shotthinggame/src/Player.java
@@ -13,6 +13,11 @@ public class Player extends Character implements KeyListener{
 		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
 			vx=5;
 		 	}
+		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
+			GameWorld.playerBullets.add(
+				new PlayerBullet(x,y,0,-10));
+			}
+		
 		}
 		public void KeyReleased(KeyEvent e) {
 			if(e.getKeyCode()==KeyEvent.VK_LEFT) {
