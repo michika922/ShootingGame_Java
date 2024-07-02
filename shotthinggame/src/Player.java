32ffa6ba2a44e67e0b288aabb5e14414bb0aa38a
takeda michika <mi.chi.ka10@icloud.com>
2024-07-02 09:51:37 +0900

    Enterキーを押すとゲームが終了する

--- shotthinggame/src/Player.java
+++ shotthinggame/src/Player.java
@@ -25,6 +25,10 @@ public class Player extends Character implements KeyListener{
 				new PlayerBullet(x,y,0,-10));
 				System.out.println("弾の数="+GameWorld.playerBullets.size());
 			}
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			System.out.println("Enterキーが押されました");
+			GameWorld.enterPressed=true;
+		}
 		
 		}
 		public void KeyReleased(KeyEvent e) {
