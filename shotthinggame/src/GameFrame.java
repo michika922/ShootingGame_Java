c9fd481eaf041a56ead0e60d49450ea08e6b52fc
takeda michika <mi.chi.ka10@icloud.com>
2024-07-02 09:55:18 +0900

    13.4Enterキーが押されたらもう一度ゲームを実行する

--- shotthinggame/src/GameFrame.java
+++ shotthinggame/src/GameFrame.java
@@ -4,7 +4,10 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player = new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
-		GameWorld.playerBullets = new Vector<PlayerBullet>();
+		while(true){
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
+				GameWorld.playerBullets = new Vector<PlayerBullet>();
 		GameWorld.enemies = new Vector<Enemy>();
 		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
 		GameWorld.enterPressed=false;
@@ -41,6 +44,7 @@ public class GameFrame extends MyFrame {
 			sleep(0.03);
 		}
 	}
+}
 
 	public void moveEnemies() {
 		for (int i = 0; i < GameWorld.enemies.size(); i++) {
